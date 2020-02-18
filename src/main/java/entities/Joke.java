3890c9356180379ec30e805d8944fe73e8ad3865
Caroline Hoeg <cph-ch465rcphbusiness.dk>
2020-02-18 10:55:22 +0100

    JokeFacade created w. TDD

---
+++ src/main/java/entities/Joke.java
@@ -0,0 +1,96 @@
+package entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+/**
+ *
+ * @author carol
+ */
+@Entity
+@NamedQueries({
+@NamedQuery(name = "Joke.deleteAllRows", query = "DELETE from Joke"),
+@NamedQuery(name = "Joke.getAll", query = "SELECT j FROM Joke j")
+})
+public class Joke implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String joke;
+    private String reference;
+    private String type;
+
+    public Joke() {
+    }
+
+    public Joke(String joke, String reference, String type) {
+        this.joke = joke;
+        this.reference = reference;
+        this.type = type;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getJoke() {
+        return joke;
+    }
+
+    public void setJoke(String joke) {
+        this.joke = joke;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Joke other = (Joke) obj;
+        if (!Objects.equals(this.joke, other.joke)) {
+            return false;
+        }
+        if (!Objects.equals(this.reference, other.reference)) {
+            return false;
+        }
+        if (!Objects.equals(this.type, other.type)) {
+            return false;
+        }
+        return true;
+    }
+
+}
