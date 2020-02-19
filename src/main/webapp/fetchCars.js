f0e3c026eb6d95c395751a779c2cbbaa500417fc
Caroline Hoeg <cph-ch465rcphbusiness.dk>
2020-02-19 13:56:46 +0100

    Filter cars

--- src/main/webapp/fetchCars.js
+++ src/main/webapp/fetchCars.js
@@ -1,5 +1,6 @@
 let url = "api/cars/all";
 let globalList;
+let cars = [];
 
 let btn = document.getElementById("fetchButton");
 btn.addEventListener("click", fetching);
@@ -10,6 +11,7 @@ function fetching() {
         //            .then(text => console.log(text))        
         .then(Response => Response.json())
         .then(data => {
+            cars = data;
             console.log("data", data);
             document.getElementById("table").innerHTML = updateTable(data);
         });
@@ -30,4 +32,34 @@ function updateTable(CarArray) {
     console.log(htmlStringValue);
     globalList = htmlStringHead + htmlStringValue;
     return htmlStringHead + htmlStringValue;
+}
+
+//Filter price
+document.getElementById("filterPriceBtn").onclick = filterPrice;
+  
+function filterPrice(evt){
+    evt.preventDefault();
+    let price = document.getElementById("priceInput");
+    let lessThanCars = cars.filter((car) => car.price < price.value);
+    document.getElementById("table").innerHTML = updateTable(lessThanCars);
+}
+
+//Filter year
+document.getElementById("filterYearBtn").onclick = filterYear;
+  
+function filterYear(evt){
+    evt.preventDefault();
+    let year = document.getElementById("yearInput");
+    let newerCars = cars.filter((car) => car.year > year.value);
+    document.getElementById("table").innerHTML = updateTable(newerCars);
+}
+
+//Filter make
+document.getElementById("filterMakeBtn").onclick = filterMake;
+  
+function filterMake(evt){
+    evt.preventDefault();
+    let make = document.getElementById("makeInput");
+    let newCars = cars.filter((car) => car.make == make.value);
+    document.getElementById("table").innerHTML = updateTable(newCars);
 }
