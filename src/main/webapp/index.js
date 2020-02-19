57c3908c8292e6c3e8a9db633a78dfb560dcef99
789Michael <allanalli@hotmail.com>
2020-02-19 11:02:12 +0100

    fixed groupmembers

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -1,35 +1,31 @@
-let table = document.getElementById("table")
-document.getElementById("btn").onclick = returnTableData;
-
 let url = "api/groupmembers/all";
-let jsondata;
+let table = document.getElementById("table")
+document.getElementById("btn").onclick = fetching;
 
-fetch(url)
-  .then(res => res.json()) //in flow1, just do it
-  .then(data => { 
-      jsondata=data;      
-})
-
-function returnTableData(){
+function fetching() {
     fetch(url)
-    table.innerHTML = maketable(jsondata);
+            .then(res => res.json()) //in flow1, just do it
+            .then(data => {
+                maketable(data);
+            })
 }
-function maketable(array){
+
+function maketable(array) {
     let returnstring = "<thead><tr>"
     let keysarray = Object.keys(array[0]);
-    keysarray.forEach(function(item){
+    keysarray.forEach(function (item) {
         returnstring += `<th>${item}</th>`
     })
     returnstring += "</tr></thead><tbody>"
-    array.forEach(function(item){
+    array.forEach(function (item) {
         let values = Object.values(item);
         returnstring += "<tr>";
-        values.forEach(function(value){
+        values.forEach(function (value) {
             returnstring += `<td>${value}</td>`;
         });
         returnstring += "</tr>";
     });
     returnstring += "</tbody>";
-    return returnstring;
+    table.innerHTML = returnstring;
 }
 
